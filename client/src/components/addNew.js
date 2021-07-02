13a03a310cc64df19973954782177acb7bd76c83
yael-shapira <yael.shapira3@gmail.com>
2021-07-02 15:20:40 +0300

    בםצצןא

--- client/src/components/addNew.js
+++ client/src/components/addNew.js
@@ -5,28 +5,29 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
  
 export default function Form() {
+  const validationSchema = Yup.object().shape({
+    fullname: Yup.string().required('Fullname is required'),
+    username: Yup.string()
+      .required('Username is required')
+      .min(6, 'Username must be at least 6 characters')
+      .max(20, 'Username must not exceed 20 characters'),
+    email: Yup.string()
+      .required('Email is required')
+      .email('Email is invalid'),
+    password: Yup.string()
+      .required('Password is required')
+      .min(6, 'Password must be at least 6 characters')
+      .max(40, 'Password must not exceed 40 characters'),
+    confirmPassword: Yup.string()
+      .required('Confirm Password is required')
+      .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
+    acceptTerms: Yup.bool().oneOf([true], 'Accept Terms is required')
+  });
  const { register, handleSubmit ,formState: { errors }
 } = useForm({
   resolver: yupResolver(validationSchema)
 });
-const validationSchema = Yup.object().shape({
-  fullname: Yup.string().required('Fullname is required'),
-  username: Yup.string()
-    .required('Username is required')
-    .min(6, 'Username must be at least 6 characters')
-    .max(20, 'Username must not exceed 20 characters'),
-  email: Yup.string()
-    .required('Email is required')
-    .email('Email is invalid'),
-  password: Yup.string()
-    .required('Password is required')
-    .min(6, 'Password must be at least 6 characters')
-    .max(40, 'Password must not exceed 40 characters'),
-  confirmPassword: Yup.string()
-    .required('Confirm Password is required')
-    .oneOf([Yup.ref('password'), null], 'Confirm Password does not match'),
-  acceptTerms: Yup.bool().oneOf([true], 'Accept Terms is required')
-});
+
  return (
    <form onSubmit={handleSubmit(data => AddNewRecord(data))} >
      <h1>New Order</h1>
