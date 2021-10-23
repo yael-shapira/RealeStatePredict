54e13fac8d9be0b3f04cf4cfc1d5ae064d8803e1
yael-shapira <yael.shapira3@gmail.com>
2021-10-23 22:33:45 +0300

    commit

--- app.py
+++ app.py
@@ -2,14 +2,18 @@ from flask import Flask
 from flask import render_template,render_template_string,request,make_response,jsonify
 import sqlite3 as sql
 import pandas as pd
-from flask_cors import CORS
+from flask_cors import cross_origin,CORS
 import pickle
 import json 
 import data.handlers.buildmodel as dm 
 import os
 from werkzeug.utils import secure_filename
+import logging
+    
 app = Flask(__name__)
 CORS(app)
+  
+
 @app.route('/api/GetRealestateInfo',methods=['GET'])
 def  GetRealestateInfo():    
     conn = sql.connect('RtateDB.db')
@@ -21,24 +25,30 @@ def  GetRealestateInfo():
     conn.close()    
     return response 
     
+
 @app.route('/api/addNewRecord',methods = ['POST', 'GET'])
 def addRow1():   
     if request.method == 'POST':    
-        json_data = request.json
-        saleValue = json_data['Sale_value_in_shekels']
-        partSold = json_data['Part_Sold']
-        area = json_data['Area']
-        rooms  = json_data['Rooms']
-        saleQuarter = json_data['Sale_Quarter']
-        saleYear = json_data['Sale_Year']
-        numBuilding = json_data['Num_Building'] 
-        sqliteConnection= sql.connect("RtateDB.db")
-        cursorObject = sqliteConnection.cursor()          
-        sqlite_insert_query = "INSERT INTO רקשךקדאשאקןמכםמק' (Sale_value_in_shekels,Part_Sold,Area,Rooms,Sale_Quarter,Sale_Year,Num_Building) VALUES (%s, %s,%s,%s,%s,%s,%s)" %(saleValue, partSold,area,rooms,saleQuarter,saleYear,numBuilding)
-        cursorObject.execute(sqlite_insert_query)
-        cursorObject.execute("COMMIT")          
-        return str(cursorObject.lastrowid)
-
+        try:
+            json_data = request.json
+            saleValue = json_data['Sale_value_in_shekels']
+            partSold = json_data['Part_Sold']
+            area = json_data['Area']
+            rooms  = json_data['Rooms']
+            saleQuarter = json_data['Sale_Quarter']
+            saleYear = json_data['Sale_Year']
+            numBuilding = json_data['Num_Building'] 
+            sqliteConnection= sql.connect("RtateDB.db")
+            cursorObject = sqliteConnection.cursor()          
+            sqlite_insert_query = "INSERT INTO realEstateInfoNew (Sale_value_in_shekels,Part_Sold,Area,Rooms,Sale_Quarter,Sale_Year,Num_Building) VALUES (%s,%s,%s,%s,%s,%s,%s)" %(saleValue,partSold,area,rooms,saleQuarter,saleYear,numBuilding)
+            cursorObject.execute(sqlite_insert_query)
+            cursorObject.execute("COMMIT")          
+            return str(cursorObject.lastrowid)  
+        except Exception as e:
+            logging.error("Exception occurred", exc_info=True)
+            return str("failed")  
+       
+   
 @app.route('/api/saveToNewTable',methods = ['POST', 'GET'])
 def addRows():   
     if request.method == 'POST':    
@@ -61,15 +71,24 @@ def addRows():
 def cleanNewTable(): 
     if request.method == 'POST':    
         df = dm.load_RealEstateNewdata()
+        dfLocality = dm.load_locality_data()
+        df = dm.prepare_realeState_data(df)
+       # dfLocality = dm.prepare_locality_data()
+        #dfGlobal = dm.prepare_global_data(df,dfLocality)
+        result = dfLocality.to_json(orient="records")
+        parsed = json.loads(result)
+        json.dumps(parsed, indent=4) 
+        response = make_response(jsonify(parsed))  
+       
         # dfLocality = dm.load_locality_data()
         # df = dm.replace_column_names(df)
         # df = dm.prepare_realeState_data(df)
         # dfGlobal = dm.prepare_global_data(df,dfLocality)
-        sqliteConnection= sql.connect("RtateDB.db")
+        # sqliteConnection= sql.connect("RtateDB.db")
         # cursorObject = sqliteConnection.cursor()
-        df.to_sql('realEstateInfoClean', con=sqliteConnection, if_exists='append')          
+        # df.to_sql('realEstateInfoClean', con=sqliteConnection, if_exists='append')          
         #return str(cursorObject.lastrowid)
-        return str("k")
+        return response
         
         # # dfLocality = dm.prepare_locality_data(dfLocality)
         # #print(check_realestate_data(df))
